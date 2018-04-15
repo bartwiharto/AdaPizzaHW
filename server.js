789bcdabee9b17dcd70f415ac9a071101990d5ae
bartwiharto <bartwiharto@gmail.com>
2018-04-15 17:44:59 -0600

    Commit 1: index page and menu works

---
+++ server.js
@@ -0,0 +1,31 @@
+//Ada's pizza
+
+// add all apps in the dependencies: body-parser, ejs, express, morgan
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const ejs = require('ejs');
+const path = require('path');
+
+
+//get the middleware for express, morgan, and bodyParser
+var app = express();
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+/* set the view engine */
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+
+//check if express runs
+
+app.get('/', (req,res) => {
+	res.send("Hello World");
+});
+
+app.listen(3000, () => 
+	console.log("Listening on port 3000"));
