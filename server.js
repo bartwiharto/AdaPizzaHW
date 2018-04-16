c92f417ba6262774be021d549dd3d63cc6738303
bartwiharto <bartwiharto@gmail.com>
2018-04-15 18:27:31 -0600

    pizza const works

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const ejs = require('ejs');
 const path = require('path');
-
+const pizza = require('./pizza.js');
 
 //get the middleware for express, morgan, and bodyParser
 var app = express();
@@ -23,9 +23,13 @@ app.set('view engine', 'ejs');
 
 //check if express runs
 
-app.get('/', (req,res) => {
-	res.send("Hello World");
-});
+// app.get('/', (req,res) => {
+// 	res.send("Hello World");
+// });
+
+app.get('*', (req, res) => {
+    res.status(404).sendFile('/404.html', {root: '404'});
+})
 
 app.listen(3000, () => 
 	console.log("Listening on port 3000"));
